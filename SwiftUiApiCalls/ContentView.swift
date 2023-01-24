fce62e4f16d01be95f68f7bdef36528d1341c18d
contestormi <anssirod@gmail.com>
2023-01-24 22:35:55 +0300

    Initial Commit

---
+++ SwiftUiApiCalls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiApiCalls
+//
+//  Created by Danila Kalish on 24.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiApiCalls/SwiftUiApiCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiApiCallsApp.swift
+//  SwiftUiApiCalls
+//
+//  Created by Danila Kalish on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiApiCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
